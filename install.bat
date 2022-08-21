5f7f52e0cc24cd2e995c56a70a6a6a9721dc626f
brover <95491506+broverz@users.noreply.github.com>
2022-08-21 20:43:30 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,4 +3,4 @@
 title Install license-gen nodejs 
 echo Install
 
-npm init -y && npm install @mcnaveen/license-gen && cls && color a && echo Install Successfully! && pause && run.bat
+npm init -y && npm install @mcnaveen/license-gen && cls && color a && echo Install Successfully! && pause && start.bat
